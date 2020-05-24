b1e177a44193e2e254568677749a297cce245347
Max <max@example.com>
2020-05-23 23:43:29 -0700

    GIT_WEB_ext Lesson 047 Revealing Elements on Scroll (Part 3)

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,15 +1,22 @@
 import throttle from "lodash/throttle"
+import debounce from "lodash/debounce"
 
 class RevealOnScroll {
-    constructor(){
-        this.itemsToReveal = document.querySelectorAll(".feature-item")
+    constructor(els, thresholdPercent){
+        this.thresholdPercent = thresholdPercent
+        this.itemsToReveal = els
+        this.browserHeight = window.innerHeight
         this.hideInitially()
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
         this.events()
     }
 
     events() {
-        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener( "scroll", this.scrollThrottle )
+        window.addEventListener( "resize", debounce( () => {
+            //console.log("resized")
+            this.browserHeight = window.innerHeight
+        }, 333 ) )
     }
 
     calcCaller() {
@@ -21,13 +28,15 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo(el) {
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100
-        console.log("test")
-        if ( scrollPercent < 75 ) {
-            el.classList.add("reveal-item--is-visible")
-            el.isRevealed = true
-            if ( el.isLastItem ) {
-                window.removeEventListener("scroll", this.scrollThrottle)
+        if ( window.scrollY + this.browserHeight > el.offsetTop ) {
+            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100
+            console.log("calculateIfScrolledTo")
+            if ( scrollPercent < this.thresholdPercent ) {
+                el.classList.add("reveal-item--is-visible")
+                el.isRevealed = true
+                if ( el.isLastItem ) {
+                    window.removeEventListener("scroll", this.scrollThrottle)
+                }
             }
         }
     }
@@ -37,6 +46,7 @@ class RevealOnScroll {
             el.classList.add("reveal-item") 
             el.isRevealed = false
         })
+        //console.log("this.itemsToReveal.length: " + this.itemsToReveal.length)
         this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
     }
 }
