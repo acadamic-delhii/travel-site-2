ff9a1e44fccd310c8752b70d434547a5b3693b3c
Max <max@example.com>
2020-05-29 23:13:28 -0700

    Bug fix at secret-area.js

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -12,7 +12,7 @@ exports.handler = function(event, context, callback) {
         body = {}
     }
 
-    if (body == "iwantyou") {
+    if (body.password == "iwantyou") {
         callback(null, {
             statusCode: 200, 
             body: secretContent
