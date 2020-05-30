d33c295b8f005e588a0533540e02ee0af44eb012
Max <max@example.com>
2020-05-29 21:16:57 -0700

    GIT_WEB_ext Lesson 062 Cloud Functions / AWS Lambda Example (Part 2)

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200, 
-        body: "OK"
-    })
+    let secretContent = `
+    <h3>Wise choice!</h3>
+    <p>Feel free to contact me</p>
+    `
+
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else{
+        body = {}
+    }
+
+    if (body == "iwantyou") {
+        callback(null, {
+            statusCode: 200, 
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
 }
